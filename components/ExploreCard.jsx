2003921ea24139b25654ec2d226356837994da7d
jecinacDarko <darkojecinac@gmail.com>
2023-03-21 03:17:48 +0100

    lint fix

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion';
 import { fadeIn } from '../utils/motion';
 import styles from '../styles';
 
-const ExploreCard = ({id, imgUrl, title, index, active, handleClick}) => (
+const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
   <motion.div
     variants={fadeIn('right', 'spring', index * 0.5, 0.6)}
     className={`relative ${
